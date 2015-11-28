14605ba3d98bc1b820ce1d72985b78448263e6b5
Yu Liang <yu.liang@thekono.com>
2015-11-29 00:40:29 +0800

    Make use of ConfigParser to read config

---
+++ config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# standard library imports
+from ConfigParser import ConfigParser
+import os.path
+
+# third party related imports
+
+# local library imports
+
+
+__all__ = ['setting']
+
+
+setting = ConfigParser()
+env = os.environ.get('ELB_LOG_ENV', 'development')
+cwd = os.path.dirname(os.path.abspath(__file__))
+setting.read(os.path.join(cwd, env + '.ini'))
