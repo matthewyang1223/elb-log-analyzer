92d722463f727d80871c405e8b60be67ff00966e
Yu Liang <yu.liang@thekono.com>
2015-12-11 20:24:47 +0800

    Implement RangeQuery by spec

---
+++ elb_log_analyzer/query/range_query.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# stadard library imports
+
+# third party related imports
+
+# local library imports
+
+
+class RangeQuery(object):
+
+    def __init__(self, field, min_val=None, max_val=None):
+
+        if min_val is None and max_val is None:
+            raise ValueError('Not a valid query')
+
+        self.field = field
+        self.min_val = min_val
+        self.max_val = max_val
+
+    def get_clause(self):
+
+        ret = {'range': {self.field: {}}}
+        obj = ret['range'][self.field]
+
+        if self.min_val is not None:
+            obj['gte'] = self.min_val
+
+        if self.max_val is not None:
+            obj['lt'] = self.max_val
+
+        return ret
