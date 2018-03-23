35ac77e65ace87914402db3084c7315422917565
Yu Liang <yu.liang@thekono.com>
2018-03-23 16:33:42 +0800

    Config file for logger

--- elb_log_analyzer/logger.py
+++ elb_log_analyzer/logger.py
@@ -2,25 +2,15 @@
 
 # standard library imports
 import logging
-from logging.handlers import TimedRotatingFileHandler
+import logging.config
 
 # third party related imports
 
 # local library imports
-from elb_log_analyzer.config import setting
+from elb_log_analyzer.config import config_file
 
 
 __all__ = ['logger']
-logging_level = getattr(logging, setting.get('logger', 'level'))
 
-handler = TimedRotatingFileHandler(
-    filename=setting.get('logger', 'filename'),
-    when='midnight',
-    backupCount=30
-)
-handler.setLevel(logging_level)
-handler.setFormatter(logging.Formatter(setting.get('logger', 'format')))
-
-logger = logging.getLogger('elb_log_analyzer')
-logger.setLevel(logging_level)
-logger.addHandler(handler)
+logging.config.fileConfig(config_file)
+logger = logging.getLogger('elb-log-analyzer')
